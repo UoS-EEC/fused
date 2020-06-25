57890c332cb500e598d45928a3c64cbf9f536c33
Sivert Sliper <sivert.sliper@arm.com>
2020-06-25 08:28:26 +0100

    Test to read & check reset values

--- test/test_Bme280.cpp
+++ test/test_Bme280.cpp
@@ -9,6 +9,7 @@
 #include <tlm_utils/simple_initiator_socket.h>
 #include <systemc>
 #include <tlm>
+#include <vector>
 #include "mcu/SpiTransactionExtension.hpp"
 #include "ps/DynamicEnergyChannel.hpp"
 #include "ps/EventLog.hpp"
@@ -47,41 +48,66 @@ SC_MODULE(tester) {
 
   void runtests() {
     spdlog::info("Testing Bme280");
-    // Prepare payload object
-    uint8_t data = 0x00;
-    tlm::tlm_generic_payload trans;
-    auto *spiExtension = new SpiTransactionExtension();
 
-    trans.set_extension(spiExtension);
-    trans.set_address(0);      // SPI doesn't use address
-    trans.set_data_length(1);  // Transfer size is 1 byte
-    trans.set_command(tlm::TLM_WRITE_COMMAND);
-    trans.set_data_ptr(&data);
+    test.nReset.write(true);
+    wait(SC_ZERO_TIME);
+
+    spdlog::info("TEST: Read & check reset values of registers");
+    sc_assert(spiRead(Bme280::ADDR_ID, 1)[0] == 0x60);
+    sc_assert(spiRead(Bme280::ADDR_RESET, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_CTRL_HUM, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_STATUS, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_CTRL_MEAS, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_CONFIG, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_PRESS_MSB, 1)[0] == 0x80);
+    sc_assert(spiRead(Bme280::ADDR_PRESS_LSB, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_PRESS_XLSB, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_TEMP_MSB, 1)[0] == 0x80);
+    sc_assert(spiRead(Bme280::ADDR_TEMP_LSB, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_TEMP_XLSB, 1)[0] == 0x00);
+    sc_assert(spiRead(Bme280::ADDR_HUM_MSB, 1)[0] == 0x80);
+    sc_assert(spiRead(Bme280::ADDR_HUM_LSB, 1)[0] == 0x00);
+
+    sc_stop();
+  }
+
+  std::vector<uint8_t> spiRead(const uint8_t addr, const size_t len) {
+    // Prepare payload object
+    uint8_t data = addr | READ_BIT;
+    tlm::tlm_generic_payload trans;
+
+    auto *spiExtension = new SpiTransactionExtension();
 
     spiExtension->clkPeriod = sc_core::sc_time(10, sc_core::SC_US);
     spiExtension->nDataBits = 8;
     spiExtension->phase = SpiTransactionExtension::SpiPhase::CAPTURE_FIRST_EDGE;
     spiExtension->polarity = SpiTransactionExtension::SpiPolarity::HIGH;
     spiExtension->bitOrder = SpiTransactionExtension::SpiBitOrder::MSB_FIRST;
+
+    trans.set_extension(spiExtension);
+    trans.set_address(0);      // SPI doesn't use address
+    trans.set_data_length(1);  // Transfer size is 1 byte
+    trans.set_command(tlm::TLM_WRITE_COMMAND);
+    trans.set_data_ptr(&data);
     sc_time delay = spiExtension->transferTime();
 
-    test.nReset.write(true);
+    // Start transfer
+    std::vector<uint8_t> response(len);
     test.chipSelect.write(false);
-    wait(SC_ZERO_TIME);
-
-    const unsigned READ_BIT = (1u << 7);
-
-    spdlog::info("TEST: Read ID register");
-    data = Bme280::ADDR_ID | READ_BIT;           // ID register address
     test.iSpiSocket->b_transport(trans, delay);  // Transfer address
     wait(delay);
-    test.iSpiSocket->b_transport(trans, delay);  // Get ID
-    wait(delay);
-    spdlog::info("Response: 0x{:08x}", spiExtension->response);
-    sc_assert(spiExtension->response == 0x60);
+    for (int i = 0; i < len; ++i) {
+      test.iSpiSocket->b_transport(trans, delay);  // Transfer data
+      response[i] = spiExtension->response;
+      wait(delay);
+    }
+    test.chipSelect.write(true);
 
-    sc_stop();
+    // delete spiExtension;
+    return response;
   }
+
+  static const unsigned READ_BIT = (1u << 7);
 
   dut test{"dut"};
 };
