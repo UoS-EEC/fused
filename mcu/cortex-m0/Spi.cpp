970d1b71b89945c79de4e5593eac7ee4a68ef5b4
Sivert Sliper <sivert.sliper@arm.com>
2020-08-17 18:28:58 +0100

    Initial commit for SPI peripheral based on the one in STM32F0xx

---
+++ mcu/cortex-m0/Spi.cpp
@@ -0,0 +1,215 @@
+/*
+ * Copyright (c) 2019-2020, University of Southampton and Contributors.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <spdlog/fmt/fmt.h>
+#include <systemc>
+#include "mcu/SpiTransactionExtension.hpp"
+#include "mcu/msp430fr5xx/Spi.hpp"
+#include "ps/EventLog.hpp"
+#include "utilities/Config.hpp"
+
+extern "C" {
+#include "mcu/msp430fr5xx/device_includes/msp430fr5994.h"
+}
+
+using namespace sc_core;
+
+Spi::Spi(sc_module_name name, const uint16_t endAddress, const sc_time delay)
+    : BusTarget(name, startAddress, endAddress, delay) {
+  // Register events
+
+  // Initialise register file
+  m_regs.addRegister(OFS_SPI_CR1, 0, RegisterFile::READ_WRITE);
+  m_regs.addRegister(OFS_SPI_CR2, 0x0700, RegisterFile::READ_WRITE);
+  m_regs.addRegister(OFS_SPI_SR, 0x0002, RegisterFile::READ_WRITE);
+  m_regs.addRegister(OFS_SPI_DR, 0, RegisterFile::READ_WRITE);
+  m_regs.addRegister(OFS_SPI_CRCPR, 0x0007, RegisterFile::READ_WRITE);
+  m_regs.addRegister(OFS_SPI_RXCRCR, 0, RegisterFile::READ_WRITE);
+  m_regs.addRegister(OFS_SPI_TXCRCR, 0, RegisterFile::READ_WRITE);
+
+  SC_METHOD(reset);
+  sensitive << pwrOn;
+
+  SC_THREAD(process);
+}
+
+void Spi::b_transport(tlm::tlm_generic_payload &trans, sc_time &delay) {
+  // Access register file
+  BusTarget::b_transport(trans, delay);
+  const auto addr = trans.get_address();
+  const auto val = m_regs.read(addr);
+  const auto len = trans.get_data_length();
+
+  if (len > 2) {
+    SC_REPORT_FATAL(this->name(),
+                    "The SPI module only accepts 8-bit and 16-but accesses.");
+  }
+
+  // Handle serial communication tasks
+  if (trans.get_command() == tlm::TLM_WRITE_COMMAND) {
+    switch (addr) {
+      case OFS_SPI_CR1:  // Control Register 1
+        m_enable = val & Spi::SPE;
+        m_enableEvent.notify(delay);
+        checkImplemented();
+        break;
+      case OFS_SPI_CR2:  // Control Register 2
+        checkImplemented();
+        break;
+      case OFS_SPI_DR:  // Data register -- push tx fifo
+        m_txFifo.put(8 * len, val);
+        m_txEvent.notify(delay);
+        break;
+      default:
+        break;
+    }
+  } else if (trans.get_command() == tlm::TLM_READ_COMMAND) {
+    switch (addr) {
+      default:
+      case OFS_SPI_DR:   // Data register -- pop rx fifo
+        if (len == 2) {  // 2 bytes
+          const auto tmp = ;
+          Utility::unpackBytes(trans.get_data_ptr(),
+                               Utility::htots(m_rxFifo.get(16)), 2);
+        } else {  // 1 byte
+          trans.get_data_ptr()[0] = m_rxFifo.get(8);
+        }
+        break;
+    }
+  }
+}
+
+void Spi::reset(void) {
+  if (pwrOn.read()) {  // Posedge of pwrOn
+    // Reset register file
+    for (uint16_t i = 0; i < m_regs.size(); i++) {
+      m_regs.write(2 * i, 0);
+    }
+    m_regs.write(OFS_UCB0CTLW0, 0x01c1);
+    m_regs.write(OFS_UCB0IFG, 0x0002);
+  }
+}
+
+void Spi::process(void) {
+  // Prepare payload object
+  tlm::tlm_generic_payload trans;
+  trans.set_command(tlm::TLM_WRITE_COMMAND);
+  std::array<uint8_t, 2> dataOut;
+  trans.set_data_ptr(dataOut.data);
+  auto *spiExtension = new SpiTransactionExtension();
+  trans.set_extension(spiExtension);
+  trans.set_address(0);  // SPI doesn't use address
+
+  wait(SC_ZERO_TIME);  // Wait for start of simulation
+
+  while (1) {
+    while (pwrOn.read() == false) {
+      wait(pwrOn.posedge_event());
+    }
+
+    unsigned cr2 = m_regs.read(OFS_SPI_CR2);
+    int nbits = ((cr2 & Spi::DS_MASK) >> Spi::DS_SHIFT) + 1;
+    if (nbits < 4) {
+      nbits = 8;
+    }
+    int nbytes = (nbits + 7) / 8;
+    bool readyToSend =
+        pwrOn.read() && m_enable && (m_txFifo.nValidBytes >= nbytes);
+
+    while (!readyToSend) {
+      wait(m_txEvent | m_enableEvent | pwrOn.posedge_event());
+      cr2 = m_regs.read(OFS_SPI_CR2);
+      nbits = ((cr2 & Spi::DS_MASK) >> Spi::DS_SHIFT) + 1;
+      if (nbits < 4) {
+        nbits = 8;
+      }
+      nbytes = (nbits + 7) / 8;
+      readyToSend =
+          pwrOn.read() && m_enable && (m_txFifo.nValidBytes >= nbytes);
+    }
+
+    // Update status register
+    m_regs.setBit(OFS_SPI_SR, BSY_SHIFT);  // Set busy flag
+
+    // Prepare  & send payload
+    const unsigned cr1 = m_regs.read(OFS_SPI_CR1);
+    spiExtension->bitOrder =
+        (cr1 & Spi::LSBFIRST_MASK)
+            ? SpiTransactionExtension::SpiBitOrder::LSB_FIRST
+            : SpiTransactionExtension::SpiBitOrder::MSB_FIRST;
+    auto baudRateDivider = 2u << ((cr1 & Spi::BR_MASK) >> Spi::BR_SHIFT);
+    spiExtension->clkPeriod = clk->getPeriod() * baudRateDivider;
+    spiExtension->polarity = (cr1 & CPOL_MASK)
+                                 ? SpiTransactionExtension::SpiPolarity::LOW
+                                 : SpiTransactionExtension::SpiPolarity::HIGH;
+    spiExtension->phase =
+        (cr1 & CPHA_MASK)
+            ? SpiTransactionExtension::SpiPhase::CAPTURE_FIRST_EDGE
+            : SpiTransactionExtension::SpiPhase::CAPTURE_SECOND_EDGE;
+
+    spiExtension->nDataBits = nbits;
+
+    Utility::unpackBytes(dataOut, m_txFifo.get(nbits), nbytes);
+    trans.set_data_length(nbytes);
+    trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+    sc_time delay = spiExtension->transferTime();
+    spiSocket->b_transport(trans, delay);
+    if (trans.is_response_error()) {
+      SC_REPORT_FATAL(this->name(), "TLM response error");
+    }
+    wait(delay);
+
+    // Receive
+    m_rxFifo.put(nbits, Utility::packBytes(spiExtension->response, nbytes));
+
+    // Update status register
+    // RXNE: when the threshold defined by FRXTH is reached
+    // TXE: when TXFIFO level is <= half capacity
+    const int FRXTH_nbytes =
+        ((cr2 & Spi::FRXTH_MASK) >> Spi::FRXTH_SHIFT) ? 1 : 2;
+    const unsigned RXNE = m_rxFifo.nValidBytes >= FRXTH_nbytes;
+    const unsigned TXE = m_txFifo.nValidBytes <= 2;
+    m_regs.write(OFS_SPI_SR, (m_txFifo.level() << FTLVL_SHIFT) |
+                                 (m_rxFifo << FRLVL_SHIFT) |
+                                 (TXE << TXE_SHIFT) | (RXNE << RXNE_SHIFT));
+
+    // Set Interrupt request
+    const bool RXNEIE = cr2 & Spi::RXNEIE_MASK;
+    const bool TXEIE = cr2 & Spi::TXEIE_MASK;
+  }
+}
+
+void Spi::checkImplemented() {
+  unsigned cr1 = m_regs.read(OFS_SPI_CR1);
+
+  if (cr1 & Spi::BIDIMODE_MASK) {
+    SC_REPORT_FATAL(
+        this->name(),
+        fmt::format(
+            "BIDIMODE set, but bidirectional data mode not implemented."));
+  }
+
+  if (cr1 & Spi::CRCEN_MASK) {
+    SC_REPORT_FATAL(
+        this->name(),
+        fmt::format(
+            "CRCEN set, but hardware CRC calculation not implemented."));
+  }
+
+  if (cr1 & Spi::CRCNEXT_MASK) {
+    SC_REPORT_FATAL(
+        this->name(),
+        fmt::format(
+            "CRCNEXT set, but hardware CRC calculation not implemented."));
+  }
+
+  if (cr1 & Spi::RXONLY_MASK) {
+    SC_REPORT_FATAL(
+        this->name(),
+        fmt::format("RXONLY set, but receive-only mode not implemented."));
+  }
+}
---
+++ mcu/cortex-m0/Spi.hpp
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2019-2020, University of Southampton and Contributors.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <tlm_utils/simple_initiator_socket.h>
+
+#include <systemc>
+#include <tlm>
+
+#include "mcu/BusTarget.hpp"
+#include "mcu/ClockSourceIf.hpp"
+#include "mcu/RegisterFile.hpp"
+#include "ps/EventLog.hpp"
+
+/**
+ * @brief The Spi class Modelling operation of Spi module, based on the one
+ * included on the STM32F0 series of microcontrollers.
+ *
+ */
+class Spi : public BusTarget {
+  SC_HAS_PROCESS(Spi);
+
+ public:
+  /* ------ Ports ------ */
+  tlm_utils::simple_initiator_socket<Spi> spiSocket{"spiSocket"};
+  sc_core::sc_port<ClockSourceConsumerIf> clk{"clk"};
+  sc_core::sc_out<bool> irq{"irq"};  //! Interrupt request output
+  sc_core::sc_in<int> returning_exception{
+      "returning_exception"};  //! Signals returning exceptions
+
+  /* ------ Register bits ------ */
+  // CR1 bits
+  static const unsigned BIDIMODE_MASK = 1u << 15;
+  static const unsigned BIDIMODE_SHIFT = 15;
+  static const unsigned BIDIOE_MASK = 1u << 14;
+  static const unsigned BIDIOE_SHIFT = 14;
+  static const unsigned CRCEN_MASK = 1u << 13;
+  static const unsigned CRCEN_SHIFT = 13;
+  static const unsigned CRCNEXT_MASK = 1u << 12;
+  static const unsigned CRCNEXT_SHIFT = 12;
+  static const unsigned CRCL_MASK = 1u << 11;
+  static const unsigned CRCL_SHIFT = 11;
+  static const unsigned RXONLY_MASK = 1u << 10;
+  static const unsigned RXONLY_SHIFT = 10;
+  static const unsigned SSM_MASK = 1u << 9;
+  static const unsigned SSM_SHIFT = 9;
+  static const unsigned SSI_MASK = 1u << 8;
+  static const unsigned SSI_SHIFT = 8;
+  static const unsigned LSBFIRST_MASK = 1u << 7;
+  static const unsigned LSBFIRST_SHIFT = 7;
+  static const unsigned SPE_MASK = 1u << 6;
+  static const unsigned SPE_SHIFT = 6;
+  static const unsigned BR_MASK = 7u << 3;
+  static const unsigned BR_SHIFT = 3;
+  static const unsigned MSTR_MASK = 1u << 2;
+  static const unsigned MSTR_SHIFT = 2;
+  static const unsigned CPOL_MASK = 1u << 1;
+  static const unsigned CPOL_SHIFT = 1;
+  static const unsigned CPHA_MASK = 1u << 0;
+  static const unsigned CPHA_SHIFT = 0;
+
+  // CR2 bits
+  static const unsigned LDMA_TX_MASK = 1u << 14;
+  static const unsigned LDMA_TX_SHIFT = 14;
+  static const unsigned LDMA_RX_MASK = 1u << 13;
+  static const unsigned LDMA_RX_SHIFT = 13;
+  static const unsigned FRXTH_MASK = 1u << 12;
+  static const unsigned FRXTH_SHIFT = 12;
+  static const unsigned DS_MASK = 0xfu << 8;
+  static const unsigned DS_SHIFT = 8;
+  static const unsigned TXEIE_MASK = 1u << 7;
+  static const unsigned TXEIE_SHIFT = 7;
+  static const unsigned RXNEIE_MASK = 1u << 6;
+  static const unsigned RXNEIE_SHIFT = 6;
+  static const unsigned ERRIE_MASK = 1u << 5;
+  static const unsigned ERRIE_SHIFT = 5;
+  static const unsigned FRF_MASK = 1u << 4;
+  static const unsigned FRF_SHIFT = 4;
+  static const unsigned NSSP_MASK = 1u << 3;
+  static const unsigned NSSP_SHIFT = 3;
+  static const unsigned SSOE_MASK = 1u << 2;
+  static const unsigned SSOE_SHIFT = 2;
+  static const unsigned TXDMAEN_MASK = 1u << 1;
+  static const unsigned TXDMAEN_SHIFT = 1;
+  static const unsigned RXDMAEN_MASK = 1u << 0;
+  static const unsigned RXDMAEN_SHIFT = 0;
+
+  // SR bits
+  static const unsigned FTLVL_MASK = 3u << 11;
+  static const unsigned FTLVL_SHIFT = 11;
+  static const unsigned FRLVL_MASK = 3u << 9;
+  static const unsigned FRLVL_SHIFT = 9;
+  static const unsigned FRE_MASK = 1u << 8;
+  static const unsigned FRE_SHIFT = 8;
+  static const unsigned BSY_MASK = 1u << 7;
+  static const unsigned BSY_SHIFT = 7;
+  static const unsigned OVR_MASK = 1u << 6;
+  static const unsigned OVR_SHIFT = 6;
+  static const unsigned MODF_MASK = 1u << 5;
+  static const unsigned MODF_SHIFT = 5;
+  static const unsigned CRCERR_MASK = 1u << 4;
+  static const unsigned CRCERR_SHIFT = 4;
+  static const unsigned TXE_MASK = 1u << 1;
+  static const unsigned TXE_SHIFT = 1;
+  static const unsigned RXE_MASK = 1u << 0;
+  static const unsigned RXE_SHIFT = 0;
+
+  /*------ Methods ------*/
+  /**
+   * @brief Spi Constructor: initialise registers
+   * @param name
+   * @param control register start address
+   * @param control register end address
+   * @param delay Bus access delay
+   */
+  Spi::Spi(sc_module_name name, const uint16_t endAddress, const sc_time delay);
+
+  /**
+   * @brief b_transport Blocking reads and writes to the config/control
+   * registers.
+   * @param trans
+   * @param delay
+   */
+  virtual void b_transport(tlm::tlm_generic_payload &trans,
+                           sc_core::sc_time &delay) override;
+
+  /**
+   * @brief reset Resets the Spi control registers to their default
+   * power-up values
+   */
+  virtual void reset(void) override;
+
+  /**
+   * @brief reset Resets the interrupt and flags only
+   */
+  virtual void swreset(void);
+
+ private:
+  /* ------ Private types ------- */
+  struct Fifo {       // 32-bit FIFO
+    int nValidBytes;  // Number of valid bytes (up to 4)
+    unsigned data;
+
+    Fifo() : nValidBytes(0), data(0) {}
+
+    unsigned get(const int nbits) {
+      sc_assert((nbits >= 4) && (nbits <= 16));  // hw spec
+      const int nbytes = ((nbits - 1) / 8) + 1;
+
+      if (nbytes > nValidBytes) {
+        spdlog::warn(
+            "Spi::Fifo::get requested {:d} bytes, but nValidBytes={:d}. "
+            "Returning all 1's.",
+            nbytes, nValidBytes);
+      }
+
+      nValidBytes -= nbytes;
+      if (nbytes == 1) {
+        const auto rv = data & 0x000000ff;
+        data >>= 8;
+        return rv;
+      } else {  // 2 bytes
+        const auto rv = data & 0x0000ffff;
+        data >>= 16;
+        return rv;
+      }
+    }
+
+    bool put(const int nbits, const unsigned val) {
+      sc_assert((nbits >= 4) && (nbits <= 16));  // hw spec
+      const int nbytes = ((nbits - 1) / 8) + 1;
+
+      if (nbytes + nValidBytes > 4) {
+        spdlog::warn(
+            "Spi::Fifo::put overflow, put {:d} bytes, but nValidBytes={:d}. "
+            "Ignoring put.",
+            nbytes, nValidBytes);
+        return 1;  // ERROR
+      }
+
+      const unsigned mask = (nbytes == 1) ? 0x000000ff : 0x0000ffff;
+      data &= ~(mask << (nValidbytes * 8));       // Clear target byte(s)
+      data |= (val & mask) << (nValidbytes * 8);  // Set target byte(s)
+      nValidBytes += nbytes;
+      return 0;  // OK
+    }
+
+    int level() const {
+      if (nValidBytes <= 2) {
+        return nValidBytes;
+      } else if (nValidBytes == 3) {
+        return 2;
+      } else {
+        return 3;
+      }
+    }
+  };
+
+  /* ------ Private variables ------ */
+  sc_core::sc_event m_txEvent{"txEvent"};
+  sc_core::sc_event m_enableEvent{"enableEvent"};
+  bool m_enable{false};
+
+  Fifo m_txFifo{0, 0, 0};
+  Fifo m_rxFifo{0, 0, 0};
+
+  /* ------ Private methods ------ */
+
+  /**
+   * @brief process Performs a serial communication type transaction
+   * with an external device; payload properties depends on the content
+   * of the config/control registers.
+   */
+  void process();
+
+  /**
+   * @brief checkImplemented Checks config registers and errors/warns if
+   * unimplemented features are enabled.
+   */
+  void Spi::checkImplemented();
+};
