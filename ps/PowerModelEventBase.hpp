640d6ba911247fa4dec23671cbc98aae317804d6
Sivert Sliper <sivert.sliper@arm.com>
2020-12-16 14:16:41 +0000

    powermodel: base class for new power model event objects

---
+++ ps/PowerModelEventBase.hpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020, University of Southampton and Contributors.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+/**
+ * Abstract base class for power model events.
+ */
+class PowerModelEventBase {
+ public:
+  //! Constructor
+  PowerModelEventBase(const std::string name_, const int id_)
+      : name(name_), id(id_) {}
+
+  /**
+   * @brief calculateEnergy calculate event energy adjusted for supply voltage.
+   */
+  virtual double calculateEnergy(double supplyVoltage) const = 0;
+
+  /* Public constants */
+  const std::string name;
+  const int id;
+};
