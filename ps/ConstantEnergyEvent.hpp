a209621bb752ae94d8fde5b5e8709bb2b1a94d86
Sivert Sliper <sivert.sliper@arm.com>
2020-12-16 14:17:24 +0000

    powermodel: implementation of constant-energy event class

---
+++ ps/ConstantEnergyEvent.hpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020, University of Southampton and Contributors.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+#include <string>
+#include "ps/PowerModelEventBase.hpp"
+
+/**
+ * Power model event for events that have a constant energy consumption
+ * regardless of supply voltage.
+ */
+class ConstantEnergyEvent : public PowerModelEventBase {
+ public:
+  //! Constructor
+  ConstantEnergyEvent(const std::string name, const int id, double energy_)
+      : PowerModelEventBase(name, id), energy(energy_) {}
+
+  virtual double calculateEnergy([
+      [maybe_unused]] const double supplyVoltage) const override {
+    return energy;
+  }
+
+  /* Public constants */
+  const double energy;
+};
